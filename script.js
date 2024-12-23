ecb47cedb4a70e885a028034c9e446743d24dddf
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 13:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
-  // your code here
-	const words = s.split(" ");
-	if (words.length === 0 || words[0] === ""){
-		return s; 
-	}
-	return words[0]; 
-	
+  const words = s.trim().split(" ");
+  
+  if (words.length === 0) {
+    return s; 
+  }
+  
+  return words[0]; 
 }
 
 // Do not change the code below
